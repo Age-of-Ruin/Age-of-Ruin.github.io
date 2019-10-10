a4fc083a850b16ca1ee0b1113c2016aca40dca37
RickC <rickmaelikconstantine@live.com>
2019-10-09 20:41:45 -0500

    Addition

--- profile/scripts/index.js
+++ profile/scripts/index.js
@@ -1,12 +1,18 @@
-import { Navbar } from "./navbar.js"
+import Navbar from './navbar.js'
 
+// // Navbar Component
 // class Navbar extends React.Component {
-  
+
 //   render() {
-//     return React.createElement("h1", null, "Hello");
+//     const home = React.createElement('li', {className: 'home'}, 'Home');
+//     const contact = React.createElement('li', {className: 'contact'}, 'Contact');
+//     const navbar = React.createElement('ul', {className: 'navbar'}, contact, home);
+
+//     return React.createElement('div', {className: 'navbarDisplay'}, navbar);
 //   }
 // }
 
+// Creates and renders all individual components
 class App extends React.Component {
   render() {
     return React.createElement(Navbar, null, null);
@@ -14,8 +20,8 @@ class App extends React.Component {
 }
   
 
-// Render all components
+// Finalize - Render App which then renders all components
 ReactDOM.render(
   React.createElement(App, null, null),
-  document.getElementById("root")
+  document.getElementById('root')
 );
