7ed04aa2c080bafdd106c00397d9c2af1b348746
RickC <rickmaelikconstantine@live.com>
2019-10-29 05:21:50 -0500

    Updated slideshow and selfee

--- profile/scripts/components/header.js
+++ profile/scripts/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component{
     }
   
     render() {
-      let pic = React.createElement('img', {className: 'header', id: 'selfee', src: 'assets/Selfee.jpg'}, null);
+      let pic = React.createElement('img', {className: 'header', id: 'selfee', src: 'assets/selfee_pro.jpg'}, null);
       let name = React.createElement('div', {className: 'header', id: 'name'}, "Richard Constantine",);
   
       let header = React.createElement('div', {className: 'header'}, name, pic);
--- profile/scripts/components/slideshow.js
+++ profile/scripts/components/slideshow.js
@@ -10,10 +10,9 @@ class Slideshow extends React.Component{
       let fullStack = React.createElement('div', {className: 'slideshow', id: 'fullstack'}, 'Full Stack Development?');
       let embeddedSystems = React.createElement('div', {className: 'slideshow', id: 'embedded'}, 'Embedded Systems?');
       let mobileApps = React.createElement('div', {className: 'slideshow', id: 'mobile'}, 'Mobile Application Development?');
-      let distributed = React.createElement('div', {className: 'slideshow', id: 'distributed'}, 'Distributed / Parallel Programming?');
-  
-  
-      let slideshow = React.createElement('div', {className: 'slideshow', id: 'slideshow'}, title, fullStack, embeddedSystems, mobileApps, distributed);
+      let webDesign = React.createElement('div', {className: 'slideshow', id: 'web'}, 'Reponsive Web Design?');
+
+      let slideshow = React.createElement('div', {className: 'slideshow', id: 'slideshow'}, title, fullStack, embeddedSystems, mobileApps, distributed, webDesign);
   
       return slideshow;
     }
